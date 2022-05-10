9f1855100d7e46a9922c29a7940c804fd90b8667
Jorge Castro <arturocastro.q@gmail.com>
2022-05-09 22:16:12 -0500

    First Commit

---
+++ classSmartphone.ts
@@ -0,0 +1,18 @@
+class Smartphone{
+    color:string;
+    brand:string;
+
+    constructor(
+    color:string,
+    brand:string
+    ){
+
+        this.color = color;
+        this.brand = brand;
+    }
+
+    makePhoneCall() :void{
+
+    }
+    
+}
