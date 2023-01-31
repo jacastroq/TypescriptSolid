3d605a41301d0d6889767dfe48db1b3930d2b714
Arturito Castro <azarturowe@hotmail.com>
2023-01-30 21:04:13 -0500

    Clase de POO con typescript

--- classSmartphone.ts
+++ classSmartphone.ts
@@ -1,18 +1,45 @@
 class Smartphone{
-    color:string;
-    brand:string;
+   private color:string;
+   private brand:string;
+   private _battery: number;
 
     constructor(
-    color:string,
-    brand:string
+    color:string
+    ,brand:string
     ){
-
         this.color = color;
         this.brand = brand;
+        this._battery = 100;
     }
 
     makePhoneCall() :void{
 
+        if (!(this._battery >=10)) {
+            throw "there isnt enough _battery";
+        }
+        else{
+
+            this._battery -= 10;
+        }
     }
     
+    get battery() :number {
+        return this._battery;
+    }
+
+    rechargeTwoTimes(){
+        this._battery = 200;
+    }
 }
+
+let newObjectPhone = new Smartphone ('verde','Samsung');
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
+newObjectPhone.makePhoneCall();
